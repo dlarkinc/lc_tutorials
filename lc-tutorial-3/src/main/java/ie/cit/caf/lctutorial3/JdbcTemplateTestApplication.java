8c6b71cc1d849ce2ec3ccc791fcb66589aeeb256
Larkin Cunningham <larkin.cunningham@gmail.com>
2015-02-12 20:24:32 +0000

    More tutorial jdbctemplate queries

--- lc-tutorial-3/src/main/java/ie/cit/caf/lctutorial3/JdbcTemplateTestApplication.java
+++ lc-tutorial-3/src/main/java/ie/cit/caf/lctutorial3/JdbcTemplateTestApplication.java
@@ -1,5 +1,8 @@
 package ie.cit.caf.lctutorial3;
 
+import ie.cit.caf.lctutorial3.domain.Artist;
+import ie.cit.caf.lctutorial3.rowmapper.ArtistRowMapper;
+
 import java.util.List;
 import java.util.Map;
 
@@ -31,9 +34,36 @@ public class JdbcTemplateTestApplication implements CommandLineRunner {
 		}
 	}
 	
+	public void query02() {
+		// Query for a list of objects - automatic mapping from row to object using RowMapper class
+		// Using parameterised "prepared statements" reduces the risk of a SQL inject attack
+		
+		System.out.println("\nQuery 2 (List male artists using RowMapper)\n-----------------");
+		
+		String sql = "SELECT * FROM artists WHERE gender = ?";
+		List<Artist> artists = jdbcTemplate.query(sql, new Object[] { "male" }, new ArtistRowMapper());
+		
+		for (Artist artist : artists) {
+			System.out.println(artist.toString());
+		}
+	}
+
+	public void query03() {
+		// Query for a specific object - automatic mapping from row to object using RowMapper class
+		
+		System.out.println("\nQuery 3 (Print artist with id 1 - uses RowMapper)\n------------------");
+		
+		String sql = "SELECT * FROM artists WHERE id = ?";
+		Artist artist = jdbcTemplate.queryForObject(sql, new Object[] { 1 }, new ArtistRowMapper());
+		
+		System.out.println(artist.toString());
+	}
+	
 	@Override
 	public void run(String... arg0) throws Exception {	
 		query01();
+		query02();
+		query03();
 	}
 	
 	public static void main(String[] args) {
--- lc-tutorial-3/src/test/java/demo/JdbcTemplateTestApplicationTests.java
+++ lc-tutorial-3/src/test/java/demo/JdbcTemplateTestApplicationTests.java
@@ -1,6 +1,6 @@
 package demo;
 
-import ie.cit.caf.lctutorial3.LcTutorial3Application;
+import ie.cit.caf.lctutorial3.JdbcTemplateTestApplication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +8,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = LcTutorial3Application.class)
-public class LcTutorial3ApplicationTests {
+@SpringApplicationConfiguration(classes = JdbcTemplateTestApplication.class)
+public class JdbcTemplateTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
