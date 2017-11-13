9cac271ac4b4a1f8f377eebd7189f97a904ae2c7
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-11-13 16:04:32 +0000

    brought redis and mongo projects forward to boot 1.5

--- MongoDB-Redis/src/main/java/ie/cit/larkin/mongoredis/DefaultConfig.java
+++ MongoDB-Redis/src/main/java/ie/cit/larkin/mongoredis/DefaultConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import ie.cit.larkin.mongoredis.entity.Artist;
--- lc-tutorial-redis/src/test/java/demo/DemoApplicationTests.java
+++ lc-tutorial-redis/src/test/java/demo/DemoApplicationTests.java
@@ -1,14 +1,10 @@
 package demo;
 
-import ie.cit.caf.redis.LcTutorialRedisApplication;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = LcTutorialRedisApplication.class)
 public class DemoApplicationTests {
 
 	@Test
